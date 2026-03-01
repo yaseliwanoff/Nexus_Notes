6b61d4a517ee5a70922049de616ab4866952f675
yarikselivanov2006-dev <yarik.selivanov2006@gmail.com>
2026-03-01 17:28:42 +0300

    update token

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,24 +2,15 @@ const admin = require('firebase-admin')
 
 let serviceAccount
 
-try {
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+} else {
   serviceAccount = require('./serviceAccountKey.json')
-} catch (e) {
-  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  } else {
-    console.error('Firebase service account not found!', e)
-    process.exit(1)
-  }
 }
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  })
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
 
 const db = admin.firestore()
-const auth = admin.auth()
-
-module.exports = { admin, db, auth }
+module.exports = { db }
